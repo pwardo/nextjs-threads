ee24d5d0df2f4ce6c83efc1a164184e26df30895
Patrick Ward <patwardo@gmail.com>
2023-09-20 16:27:12 +0100

    Fill out layout components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
+import '../globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
-import '../globals.css';
 
 export const metadata = {
   title: 'NextJS Threads',
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import '../globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { Inter } from 'next/font/google';
 
 import Topbar from '@/components/shared/Topbar';
 import LeftSidebar from '@/components/shared/LeftSidebar';
@@ -11,6 +12,7 @@ export const metadata = {
   description: 'Threads App built with NextJS 13.4, Tailwind CSS and MongoDB',
 };
 
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -20,17 +22,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <Topbar />
-          <main>
-            <LeftSidebar />
-              <section className='main-container'>
-                <div className='w-full max-w-4xl'>
-                  {children}                  
-                </div>
-              </section>
-            <RightSidebar />
-          </main>
-        <Bottombar />
+        <body className={inter.className}>
+          <Topbar />
+            <main className='flex flex-row'>
+              <LeftSidebar />
+                <section className='main-container'>
+                  <div className='w-full max-w-4xl'>
+                    {children}                  
+                  </div>
+                </section>
+              <RightSidebar />
+            </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
