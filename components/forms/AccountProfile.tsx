f4619766ee680746fb3453444a9ad9d1c0585df3
Patrick Ward <patwardo@gmail.com>
2023-09-21 12:14:23 +0100

    Handle onSubmit (ui|frontend)

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import { Button } from "@/components/ui/button";
 
 import { UserValidation } from '@/lib/validations/user';
 import { ChangeEvent, useState } from 'react';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
 
 interface Props {
   user: {
@@ -34,6 +36,8 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props ) => {
   const [files, setFiles] = useState<File[]>([]);
+
+  const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -65,10 +69,19 @@ const AccountProfile = ({ user, btnTitle }: Props ) => {
     }
   }
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+
+    if(hasImageChanged) {
+      const imgRes = await startUpload(files);
+
+      if(imgRes && imgRes[0].url) {
+        values.profile_photo = imgRes[0].url;
+      }
+    }
+
+    // TODO: update user profile
   }
 
   return (
