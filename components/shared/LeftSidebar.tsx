c3482306ea1d8b92adee29fed10e474a0daf2e1f
Patrick Ward <patwardo@gmail.com>
2023-09-25 21:02:42 +0100

    Fix left sidebar link to profile

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,12 +3,13 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
 
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -19,6 +20,8 @@ function LeftSidebar() {
               pathname.includes(link.route) &&
               link.route.length > 1
             ) || pathname === link.route;
+
+            if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
             return (
               <Link 
