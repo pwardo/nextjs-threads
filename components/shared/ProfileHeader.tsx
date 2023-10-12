ccb52059f86bd85635b8dfa184023ba9a9b69abc
Patrick Ward <patwardo@gmail.com>
2023-10-12 14:02:43 +0100

    Add user porfile edit page

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
   accountId: string;
@@ -26,9 +27,9 @@ const ProfileHeader = ({
           <div className="relative w-20 h-20 object-cover">
             <Image
               src={imgUrl}
-              alt={name}
+              alt={`Profile picture of ${name}`}
               fill
-              className="rounded-full object-cover shadow-2xl" 
+              className="rounded-full object-cover shadow-2xl"
             />
           </div>
 
@@ -37,6 +38,20 @@ const ProfileHeader = ({
             <p className="text-base-medium text-gray-1">@{username}</p>
           </div>
         </div>
+        {accountId === authUserId && type !== "Community" && (
+          <Link href='/profile/edit'>
+            <div className='flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2'>
+              <Image
+                src='/assets/edit.svg'
+                alt='logout'
+                width={16}
+                height={16}
+              />
+
+              <p className='text-light-2 max-sm:hidden'>Edit</p>
+            </div>
+          </Link>
+        )}
       </div>
 
       <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
