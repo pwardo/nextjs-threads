09c0fc6858d288bb5362999d25c05eff9db3abc0
Patrick Ward <patwardo@gmail.com>
2023-10-14 13:01:24 +0100

    Upgrade Next version to v13.5.4-canary.6

--- lib/testUtils/app-router-context-provider-mock.tsx
+++ lib/testUtils/app-router-context-provider-mock.tsx
@@ -5,7 +5,7 @@
 import {
   AppRouterContext,
   AppRouterInstance,
-} from 'next/dist/shared/lib/app-router-context';
+} from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import React from 'react';
 
 export type AppRouterContextProviderMockProps = {
